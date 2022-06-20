74deadbddfb449806ede6e3d7f7ce42295fca594
Allana Karla <allanakarla1702@gmail.com>
2022-06-20 18:42:21 -0300

    :sparkles: feat: tabela 'PostCategories'

---
+++ src/database/migrations/20220620191450-create-postCategories.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostCategories', { 
+      postId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'BlogPosts',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        primaryKey: true
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        primaryKey: true
+      },
+     });
+    
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('PostCategories');
+  
+  }
+};
