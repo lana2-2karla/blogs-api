501d0625e5f11cd301d444a95b4a08965214466f
Allana Karla <allanakarla1702@gmail.com>
2022-06-20 18:36:48 -0300

    :sparkles: feat: router e middleware de erro

--- src/api.js
+++ src/api.js
@@ -1,12 +1,14 @@
 const express = require('express');
-
-// ...
+require('express-async-errors');
+const errorMiddleware = require('../middlewares/error');
 
 const app = express();
 
 app.use(express.json());
 
-// ...
+app.use(require('../router'));
+
+app.use(errorMiddleware);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
