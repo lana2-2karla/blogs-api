f460c40bfcd6293ef75e6066d2e9e7354890686a
Allana Karla <allanakarla1702@gmail.com>
2022-06-21 11:43:19 -0300

    :truck: refactor: referente a arquivos movidos

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 const express = require('express');
 require('express-async-errors');
-const errorMiddleware = require('../middlewares/error');
+const errorMiddleware = require('./middlewares/error');
 
 const app = express();
 
 app.use(express.json());
 
-app.use(require('../router'));
+app.use(require('./router'));
 
 app.use(errorMiddleware);
 
