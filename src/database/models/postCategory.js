82cdcc730566828d3f5191a3dfe18fc4c1b182b7
Allana Karla <allanakarla1702@gmail.com>
2022-06-21 11:47:56 -0300

    :sparkles: feat: modelo de tabela 'BlogPost'

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,30 @@
+const postCategoriesSchema = (sequelize, DataTypes) => {
+    const postCategoriesTable = sequelize.define('PostCategory', {
+        postId: DataTypes.INTEGER,
+        categoryId: DataTypes.INTEGER
+    },
+    { timestamps: false, tableName: 'PostCategories' }
+    );
+
+    postCategoriesTable.associate = (models) => {
+
+        models.BlogPost.belongsToMany(models.Category, {
+            through: postCategoriesTable,
+            foreignKey: 'postId',
+            otherKey: 'categoryId',
+            as: 'categories'
+        });
+        
+        models.Category.belongsToMany(models.BlogPost, {
+            through: postCategoriesTable,
+            foreignKey: 'categoryId',
+            otherKey: "postId",
+            as: 'posts'
+        });
+    }
+    return postCategoriesTable;
+};
+
+module.exports = postCategoriesSchema;
+
+// relacionamento entre tabelas - referência: https://tableless.com.br/sequelize-a-solu%C3%A7%C3%A3o-para-seus-relacionamentos/
