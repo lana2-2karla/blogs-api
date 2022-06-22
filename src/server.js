1fb7bd86ae53de5bfcd867d5ba0a41173fe81746
Allana Karla <allanakarla1702@gmail.com>
2022-06-22 12:06:55 -0300

    :truck: refactor: app.use router/errorMiddleware

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const app = require('./api');
+const errorMiddleware = require('./middlewares/error');
 
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
@@ -8,5 +9,9 @@ const port = process.env.API_PORT || 3000;
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use(require('./router'));
+
+app.use(errorMiddleware);
 
 app.listen(port, () => console.log('ouvindo porta', port));
