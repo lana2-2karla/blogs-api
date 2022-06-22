de27da470649a98e5c5e2525b2036b4d0bf7529d
Allana Karla <allanakarla1702@gmail.com>
2022-06-22 19:25:12 -0300

    :sparkle: feat: regra de negócio - login

---
+++ src/services/login.service.js
@@ -0,0 +1,17 @@
+const { getUserLoginEmailModel, addUserLoginModel } = require('../models/login.model');
+
+const addUserLoginService = async (userData) => {
+    const status = {
+        status: 400,
+        message: 'Invalid fields',
+    };
+    const userLogin = await getUserLoginEmailModel(userData);
+    if (!userLogin) throw status;
+
+    const token = addUserLoginModel(userData);
+    return token;
+};
+
+module.exports = { 
+    addUserLoginService,
+};
