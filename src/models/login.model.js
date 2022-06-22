92182a47370cbb2101ae883b30543d3b3ef2f217
Allana Karla <allanakarla1702@gmail.com>
2022-06-22 19:23:26 -0300

    :sparkles: feat: captura usuário e gera token

---
+++ src/models/login.model.js
@@ -0,0 +1,22 @@
+const { User } = require('../database/models');
+const { generateJWTToken } = require('../utils/jwtToken');
+
+
+const getUserLoginEmailModel = async (userData) => {
+    const user = await User.findOne({
+        where: {
+            email: userData.email,
+        }
+    });
+    return user;
+};
+
+const addUserLoginModel = async (userData) => {
+    const generateToken = generateJWTToken(userData);
+    return generateToken;
+};
+
+module.exports = { 
+    getUserLoginEmailModel,
+    addUserLoginModel,
+};
