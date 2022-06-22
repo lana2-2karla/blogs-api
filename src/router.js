e9f199c3ff81f51d0dd66a2c9b9bb6b58fa9d1b1
Allana Karla <allanakarla1702@gmail.com>
2022-06-22 19:30:40 -0300

    :sparkles: feat: método POST - rota /login

--- src/router.js
+++ src/router.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const { addUserLoginController } = require('./controllers/login.controller');
 const { addUsersController } = require('./controllers/users.controller');
 const usersValidateMiddleware = require('./middlewares/users');
+const usersLoginMiddleware = require('./middlewares/login');
 
 const router = express.Router();
+
+router.post('/login', usersLoginMiddleware, addUserLoginController);
 
 router.post('/user', usersValidateMiddleware, addUsersController);
 
