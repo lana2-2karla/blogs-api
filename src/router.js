ac1eb512bf7457e3d86a5ae70f7458cbc7fb20f4
Allana Karla <allanakarla1702@gmail.com>
2022-06-23 19:04:35 -0300

    sparkles: feat: método GET - rota user/:id

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { addUserLoginController } = require('./controllers/login.controller');
-const { addUsersController, getUsersAllController } = require('./controllers/users.controller');
+const { addUsersController, 
+    getUsersAllController, 
+    getUserByIdController,
+} = require('./controllers/users.controller');
 const usersValidateMiddleware = require('./middlewares/users');
 const usersLoginMiddleware = require('./middlewares/login');
 const authenticationToken = require('./middlewares/authToken');
@@ -12,5 +15,7 @@ router.post('/login', usersLoginMiddleware, addUserLoginController);
 router.post('/user', usersValidateMiddleware, addUsersController);
 
 router.get('/user', authenticationToken, getUsersAllController);
+
+router.get('/user/:id', authenticationToken, getUserByIdController);
 
 module.exports = router;
