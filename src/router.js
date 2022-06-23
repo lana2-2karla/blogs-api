f6ec7e1f5b946b8235e42077a0b8b044589d3ac7
Allana Karla <allanakarla1702@gmail.com>
2022-06-23 20:14:02 -0300

    :sparkles: feat: métodod POST - rota /categories

--- src/router.js
+++ src/router.js
@@ -7,6 +7,8 @@ const { addUsersController,
 const usersValidateMiddleware = require('./middlewares/users');
 const usersLoginMiddleware = require('./middlewares/login');
 const authenticationToken = require('./middlewares/authToken');
+const categoriesValidation = require('./middlewares/categories');
+const { addCategoryController } = require('./controllers/categories.controller');
 
 const router = express.Router();
 
@@ -17,5 +19,7 @@ router.post('/user', usersValidateMiddleware, addUsersController);
 router.get('/user', authenticationToken, getUsersAllController);
 
 router.get('/user/:id', authenticationToken, getUserByIdController);
+
+router.post('/categories', authenticationToken, categoriesValidation, addCategoryController);
 
 module.exports = router;
