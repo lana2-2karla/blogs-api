2929e4cb5bcc8ec54ae35b4271906713ed59bf8f
Allana Karla <allanakarla1702@gmail.com>
2022-06-23 18:04:15 -0300

    :sparkles: feat: método GET - rota /user

--- src/router.js
+++ src/router.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const { addUserLoginController } = require('./controllers/login.controller');
-const { addUsersController } = require('./controllers/users.controller');
+const { addUsersController, getUsersAllController } = require('./controllers/users.controller');
 const usersValidateMiddleware = require('./middlewares/users');
 const usersLoginMiddleware = require('./middlewares/login');
+const authenticationToken = require('./middlewares/authToken');
 
 const router = express.Router();
 
 router.post('/login', usersLoginMiddleware, addUserLoginController);
 
 router.post('/user', usersValidateMiddleware, addUsersController);
+
+router.get('/user', authenticationToken, getUsersAllController);
 
 module.exports = router;
