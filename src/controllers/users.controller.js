e69510f2bc9fe4fec15d446eccd2840a35d3c9e0
Allana Karla <allanakarla1702@gmail.com>
2022-06-23 18:02:45 -0300

    :sparkles: feat: controle de captura dos usuários

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,8 +1,16 @@
-const { addUserService } = require('../services/users.service');
+const { addUserService, getUsersAllService } = require('../services/users.service');
 
 const addUsersController = async (req, res) => {
     const token = await addUserService(req.body);
     res.status(201).json({ token });
 };
 
-module.exports = { addUsersController };
+const getUsersAllController = async (_req, res) => {
+    const users = await getUsersAllService();
+    res.status(200).json(users);
+};
+
+module.exports = { 
+    addUsersController, 
+    getUsersAllController,
+};
