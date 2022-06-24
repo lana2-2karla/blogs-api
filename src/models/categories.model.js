bb8d6d7baaa333d6ba7567f0c78252b80bccf584
Allana Karla <allanakarla1702@gmail.com>
2022-06-24 16:57:49 -0300

    :sparkles: feat: captura categorias

--- src/models/categories.model.js
+++ src/models/categories.model.js
@@ -1,10 +1,17 @@
 const { Category } = require('../database/models');
 
+
+const getCategoryAllModel = async () => {
+    const categories = await Category.findAll();
+    return categories;
+}
+
 const addCategoryModel = async (categoryData) => {
     const categoryCreated = await Category.create(categoryData);
     return categoryCreated;
 };
 
 module.exports = {
+    getCategoryAllModel,
     addCategoryModel,
 }
