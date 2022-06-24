3e8423f57963a1e73a2170b15bb6481be6efa4b8
Allana Karla <allanakarla1702@gmail.com>
2022-06-24 16:59:30 -0300

    :sparkles: feat: controle de captura - categories

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -1,8 +1,16 @@
-const { addCategoryService } = require('../services/categories.service');
+const { addCategoryService, getCategoryAllService } = require('../services/categories.service');
+
+const getCategoryAllController = async (_req, res) => {
+    const categories = await getCategoryAllService();
+    res.status(200).json(categories);
+};
 
 const addCategoryController = async (req, res) => {
     const category = await addCategoryService(req.body);
     res.status(201).json(category);
 };
 
-module.exports = { addCategoryController };
+module.exports = { 
+    getCategoryAllController,
+    addCategoryController,
+};
