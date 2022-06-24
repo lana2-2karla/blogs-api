1ef8b28386dc97d0caa33e4c549e0666df46ced4
Allana Karla <allanakarla1702@gmail.com>
2022-06-24 17:00:35 -0300

    :sparkles: feat: método GET - rota /categories

--- src/router.js
+++ src/router.js
@@ -1,14 +1,20 @@
 const express = require('express');
-const { addUserLoginController } = require('./controllers/login.controller');
-const { addUsersController, 
-    getUsersAllController, 
-    getUserByIdController,
-} = require('./controllers/users.controller');
 const usersValidateMiddleware = require('./middlewares/users');
 const usersLoginMiddleware = require('./middlewares/login');
 const authenticationToken = require('./middlewares/authToken');
 const categoriesValidation = require('./middlewares/categories');
-const { addCategoryController } = require('./controllers/categories.controller');
+
+const { addUserLoginController } = require('./controllers/login.controller');
+
+const { 
+    addUsersController, 
+    getUsersAllController, 
+    getUserByIdController,
+} = require('./controllers/users.controller');
+
+const { 
+    addCategoryController, 
+    getCategoryAllController } = require('./controllers/categories.controller');
 
 const router = express.Router();
 
@@ -21,5 +27,7 @@ router.get('/user', authenticationToken, getUsersAllController);
 router.get('/user/:id', authenticationToken, getUserByIdController);
 
 router.post('/categories', authenticationToken, categoriesValidation, addCategoryController);
+
+router.get('/categories', authenticationToken, getCategoryAllController);
 
 module.exports = router;
