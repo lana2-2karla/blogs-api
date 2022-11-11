053984ea3cf3c84a12dca55f7269364477893df9
lana2-2karla <allanakarla1702@gmail.com>
2022-11-11 15:55:45 -0300

    :sparkles: docs: configuração do swagger

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const app = require('./api');
 const errorMiddleware = require('./middlewares/error');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json')
 
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
@@ -13,5 +15,10 @@ app.get('/', (_request, response) => {
 app.use(require('./router'));
 
 app.use(errorMiddleware);
+
+//swaggerUI - link que usei de referência: https://www.youtube.com/watch?v=WhFx2heoFrA&t=2277s
+//link da documentação - http://localhost:3000/api-docs
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.listen(port, () => console.log('ouvindo porta', port));
